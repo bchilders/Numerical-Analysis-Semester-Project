620a4ab1c2cc02e60c979378b7ac6c4e2b9e197a
Patrick Emami <patrickemami@gmail.com>
2015-04-26 15:45:00 -0400

    Added parser'

--- src/linear_least_squares.py
+++ src/linear_least_squares.py
@@ -3,13 +3,15 @@ __author__ = 'patrickemami'
 import os, csv
 
 CONFIG_DIR = 'config'
-DATA = 'iris.data.txt'
+DATA_STORE = 'iris.data.txt'
 
 dir = os.path.dirname(__file__)
-cfg_file = os.path.join(dir, '..', CONFIG_DIR, DATA)
+cfg_file = os.path.join(dir, '..', CONFIG_DIR, DATA_STORE)
 
-def init():
+def parse():
     with open(cfg_file, "r") as f:
-        dimensions = f.readline().strip().split(',')
-        for line in f:
-            map_text.append(line.strip())
+        data = csv.reader(f)
+        return data
+
+if __name__ == '__main__':
+    parse()
