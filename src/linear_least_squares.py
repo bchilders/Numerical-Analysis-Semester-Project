dd9c9c09fef018a5b81bad377c1a28d1f27d801f
Patrick Emami <patrickemami@gmail.com>
2015-04-26 16:38:47 -0400

    Added do_feature_vector

--- src/linear_least_squares.py
+++ src/linear_least_squares.py
@@ -4,14 +4,29 @@ import os, csv
 
 CONFIG_DIR = 'config'
 DATA_STORE = 'iris.data.txt'
+NUMBER_OF_ATTRIBUTES = 4
+NUMBER_OF_INSTANCES = 150
 
 dir = os.path.dirname(__file__)
 cfg_file = os.path.join(dir, '..', CONFIG_DIR, DATA_STORE)
 
 def parse():
+    data = []
     with open(cfg_file, "r") as f:
-        data = csv.reader(f)
-        return data
+        reader = csv.reader(f)
+        for line in reader:
+            data.append(list(line))
+    print data
+    return data
+
+def do_feature_vector(data):
+    x = []
+    for row in data:
+        list_of_floats = [float(_) for _ in row[:NUMBER_OF_ATTRIBUTES]]
+        x.append([1.0] + list_of_floats)
+    return x
 
 if __name__ == '__main__':
-    parse()
+    data = parse()
+    x = do_feature_vector(data)
+    print x
