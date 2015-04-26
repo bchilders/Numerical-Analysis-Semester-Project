a74332fcfd74da3a4ed3b133b4f5faae0be2ca79
inukai132 <tylord1@gmail.com>
2015-04-26 16:20:12 -0400

    Classification Function

--- src/linear_least_squares.py
+++ src/linear_least_squares.py
@@ -5,13 +5,29 @@ import os, csv
 CONFIG_DIR = 'config'
 DATA_STORE = 'iris.data.txt'
 
+CLASS_1 = 'Iris-setosa'
+CLASS_n1 = 'Iris-versicolor'
+
 dir = os.path.dirname(__file__)
 cfg_file = os.path.join(dir, '..', CONFIG_DIR, DATA_STORE)
 
 def parse():
     with open(cfg_file, "r") as f:
         data = csv.reader(f)
+#        for _ in data:
+#            print _
         return data
 
+def classify(v):
+    for _ in v:
+        if _[-1] == CLASS_1:
+            _[-1] = 1
+        elif _[-1] == CLASS_n1:
+            _[-1] = -1
+        else:
+            _[-1] = 0
+    return v
+
 if __name__ == '__main__':
-    parse()
+    parsed = parse()
+    #classify(parsed)
